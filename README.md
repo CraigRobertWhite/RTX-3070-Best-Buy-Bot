c15bcf9ff93899fbebae91079834061e145f3470
Craig White <craigwhiteofficial@gmail.com>
2021-06-28 20:48:15 -0400

    Revamped project
    - Fixed button links
    - Replaced info.py with .env
    - Added Pipfile
    - Added .gitignore
    - Updated README.md

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # RTX 3070 Best Buy Bot
 
 ## Features
-- Refreshes link until "Add to Cart" button is available
-- Automate entire checkout process
+- Automatically refreshes product page until available then completes the checkout process
 
 ## Prerequisites
 - Sign up for a Best Buy account
-- Add all billing/shipping info to your account (must only have one card on the account)
+- Ensure that only one card is on your account
+- Add shipping address to your account
 
 ## Dependencies
-- Selenium
-	- `pip install selenium`
+- [Python 3.9](https://www.python.org/downloads/release/python-390/)
+- [pipenv](https://pypi.org/project/pipenv/)
 - [Google Chrome](https://www.google.com/chrome/)
 - [ChromeDriver](https://chromedriver.chromium.org/downloads)
-	- Extract the compressed chromedriver executable to a driectory of your choice (be sure to update the path in bot.py accordingly)
-		- For the Windows machine I developed this on, I chose `C:\Program Files (x86)\ChromeDriver\chromedriver.exe`
+	- Extract the compressed chromedriver executable to a directory of your choice
 
 ## Running the Bot
-1. Make sure you have [Python 3.9](https://www.python.org/downloads/release/python-390/)
-2. Edit the info.py file with your Best Buy account email and password and cvv for the card on the account
-3. Navigate to your project directory and run the bot.py script from your preferred environment
-4. Feel free to change the Best Buy links in the bot.py file to any item on bestbuy.com
-	- **NOTE:** From testing it appears that it depends on the price of the item wheter or not the cvv number is asked for. If you plan to use this bot for something cheaper you may need to remove/comment the segment of code detecting and filling in the cvv field.
-
-
+1. Navigate to project directory
+2. Copy `.env.example` to `.env` and replace the example data with your own
+3. Install python dependencies (this will create a new virtual environment when run for the first time)
+	```sh
+	pipenv sync
+	```
+4. Enter environment
+	```sh
+	pipenv shell
+	```
+5. Run application
+	```sh
+	python bot.py
+	```
--- bot.py
+++ bot.py
@@ -2,86 +2,60 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from decouple import config
 
-import info
+item = input('Item URL: ')
+driver = webdriver.Chrome(config('CHROME_DRIVER_PATH'))
+driver.get(item)
 
-# make sure this path is correct
-PATH = "C:\Program Files (x86)\ChromeDriver\chromedriver.exe"
-
-driver = webdriver.Chrome(PATH)
-
-RTX3070LINK1 = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
-RTX3070LINK2 = "https://www.bestbuy.com/site/gigabyte-geforce-rtx-3070-8g-gddr6-pci-express-4-0-graphics-card-black/6437912.p?skuId=6437912"
-XBOXONETEST = "https://www.bestbuy.com/site/microsoft-xbox-one-s-1tb-console-bundle-white/6415222.p?skuId=6415222"
-
-driver.get(RTX3070LINK1)
-
-isComplete = False
-
-while not isComplete:
-    # find add to cart button
+while True:
+    # Add to Cart ------------------------------------------------------------------------------------------------------
     try:
-        atcBtn = WebDriverWait(driver, 10).until(
+        add_to_cart_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".add-to-cart-button"))
         )
-    except:
+    except Exception:
         driver.refresh()
         continue
-
     print("Add to cart button found")
+    add_to_cart_button.click()
 
-    try:
-        # add to cart
-        atcBtn.click()
+    # Go to Cart -------------------------------------------------------------------------------------------------------
+    driver.get("https://www.bestbuy.com/cart")
 
-        # go to cart and begin checkout as guest
-        driver.get("https://www.bestbuy.com/cart")
+    # Go to Checkout ---------------------------------------------------------------------------------------------------
+    checkout_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, ".checkout-buttons__checkout"))
+    )
+    checkout_button.click()
+    print("Beginning checkout")
 
-        checkoutBtn = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button"))
-        )
-        checkoutBtn.click()
-        print("Successfully added to cart - beginning check out")
+    # Login ------------------------------------------------------------------------------------------------------------
+    email_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "fld-e"))
+    )
+    email_input.send_keys(config('EMAIL'))
 
-        # fill in email and password
-        emailField = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "fld-e"))
-        )
-        emailField.send_keys(info.email)
+    password_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "fld-p1"))
+    )
+    password_input.send_keys(config('PASSWORD'))
 
-        pwField = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "fld-p1"))
-        )
-        pwField.send_keys(info.password)
+    sign_in_button = driver.find_element_by_class_name("btn-lg")
+    sign_in_button.click()
+    print("Logging in")
 
-        # click sign in button
-        signInBtn = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[3]/button"))
-        )
-        signInBtn.click()
-        print("Signing in")
+    # Populate CVV -----------------------------------------------------------------------------------------------------
+    cvv_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "cvv"))
+    )
+    cvv_input.send_keys(config('CVV'))
+    print("Placing order")
 
-        # fill in card cvv
-        cvvField = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "credit-card-cvv"))
-        )
-        cvvField.send_keys(info.cvv)
-        print("Attempting to place order")
+    # Place Order ------------------------------------------------------------------------------------------------------
+    place_order_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".button__fast-track"))
+    )
+    place_order_button.click()
 
-        # place order
-        placeOrderBtn = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, ".button__fast-track"))
-        )
-        placeOrderBtn.click()
-
-        isComplete = True
-    except:
-        # make sure this link is the same as the link passed to driver.get() before looping
-        driver.get(RTX3070LINK1)
-        print("Error - restarting bot")
-        continue
-
-print("Order successfully placed")
-
-
-
+    raise Exception(f"Bought: {item}")
